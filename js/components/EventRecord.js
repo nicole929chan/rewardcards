09cc4c633bae1847e2cccad74895f833833d72e2
Nicole <nicole929chan@gmail.com>
2023-04-11 17:54:19 +0800

    Record, WIP...

--- js/components/EventRecord.js
+++ js/components/EventRecord.js
@@ -15,6 +15,17 @@ const EventRecord = {
         <div class="text-black">
           <div><span class="points">xx</span> 點 </div>
         </div>
+        <div class="record-box bg-white horizon align-center px-22 w-100">
+          <div class="w-30 pr-22" style="border-right: 1px solid #E7E4E2;">
+            <span>+</span>
+            <img src="../img/icons/icon_gold.png" style="width: 32px;" />
+            <span class="points">2</span>
+          </div>
+          <div class="px-22">
+            <div class="text-300_">台北101旗艦店</div>
+            <div class="text-400_ mt-12">2023/01/01</div>
+          </div>
+        </div>
       </div>
       <div class="horizon justify-center w-100 mt-44">
         <div class="w-50" @click="toHome">
